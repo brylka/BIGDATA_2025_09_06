1377e94550ed7480af79303a92a5915dfc3e04bd
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-06 11:25:29 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Witaj świecie!!!!!"
+    temperatura = 50
+    return render_template('index.html', temp=temperatura)
+
+@app.route('/hello')
+def hello():
+    temp = 66
+    return f"Temp: {temp}"
 
 if __name__ == '__main__':
     app.run(debug=True)
