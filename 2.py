92c738e3fd1c7b4470f556574fdcfbc4acde16ca
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-06 14:30:53 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+import base64
 import json
 
 from flask import Flask, render_template, request
@@ -45,6 +46,33 @@ def hello_world():
     models = ['gpt-4o-mini', 'gpt-4.1-nano', 'gpt-5-nano', 'gpt-5']
 
     return render_template('index.html', messages=messages, messages_json=json.dumps(messages), models=models)
+
+@app.route('/ocr', methods=['GET', 'POST'])
+def ocr():
+    if request.method == 'POST':
+        file = request.files['image']
+        #file.save('plik_otrzymany.png')
+
+        file_content = file.read()
+        base64_image = base64.b64encode(file_content).decode('utf-8')
+
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "Wykonaj OCR na tym pliku. Wypisz tekst, który jest widoczny na obrazku."},
+                        {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image}"}}
+                    ]
+                }
+            ]
+        )
+
+        assistant_response = response.choices[0].message.content
+        print(assistant_response)
+
+    return render_template('ocr.html', ocr_text=assistant_response)
 
 
 if __name__ == '__main__':
