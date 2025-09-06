1479ae7cb44387f150f339978257c44840e77bd1
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-06 12:31:31 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from openai import OpenAI
+
+def get_api_key():
+    with open('openai_key.txt', 'r') as f:
+        return f.read().strip()
 
 app = Flask(__name__)
+client = OpenAI(api_key=get_api_key())
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    temperatura = 50
-    return render_template('index.html', temp=temperatura)
+    messages = []
 
-@app.route('/hello')
-def hello():
-    temp = 66
-    return f"Temp: {temp}"
+    if request.method == 'POST':
+        user_prompt = request.form.get('prompt')
+        #print(f"Odebrałem: {user_prompt}")
+        messages.append({"role": "user", "content": user_prompt})
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages
+        )
+        assistant_response = response.choices[0].message.content
+        print(assistant_response)
+
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
