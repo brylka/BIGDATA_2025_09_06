2601ee7536738c74e70899cd22150adc250c7161
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-07 12:45:48 +0200

    Niedziela

--- 6.py
+++ 6.py
@@ -19,5 +19,35 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Inicjacja skalera do skalowania danych
 scaler = StandardScaler()
+# Sakolowanie danych treningowych
 X_train = scaler.fit_transform(X_train)
+# Skalowanie danych testowych
 X_test = scaler.transform(X_test)
+
+# Tworzenie modelu z użyciem funkcyjnego API Keras
+inputs = Input(shape=(X_train.shape[1],))
+x = Dense(64, activation='relu')(inputs)
+x = Dense(32, activation='relu')(x)
+outputs = Dense(1)(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+# Kompilacja modelu z optymalizatorem Adam i funkcją straty
+model.compile(optimizer='Adam', loss="mean_squared_error")
+
+# Wyświetlenie podsumowania modelu
+model.summary()
+
+# Trenowanie modelu z 100 epokami i walidacją na 10% danych treningowych
+history = model.fit(X_train, y_train, epochs=100, validation_split=0.1)
+
+# Utworzenie wykresu straty dla danych treningowych i walidacyjnych
+plt.figure(figsize=(10, 6))
+plt.plot(history.history['loss'], label='Traing Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('loss.plot.png')
+plt.close()
+
+
