a4231e99ddac4790b57cba4cc234cf8fd12f466a
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-07 13:05:28 +0200

    Niedziela

--- 6.py
+++ 6.py
@@ -50,4 +50,33 @@ plt.legend()
 plt.savefig('loss.plot.png')
 plt.close()
 
+# Przewidywanie wartości
+y_pred = model.predict(X_test)
+# Obliczanie błędu średniokwadratowego dla danych testowych
+mse = tf.keras.losses.MeanSquaredError()
+mse_value = mse(y_test, y_pred).numpy()
+print(f"Mean squared error: {mse_value}")
 
+# Dane nowej nieruchomości
+new_property = {
+    'MedInc': 8.32,
+    'HouseAge': 25,
+    'AveRooms': 6.23,
+    'AveBedrms': 1.01,
+    'Population': 1800,
+    'AveOccup': 3.5,
+    'Latitude': 37.88,
+    'Longitude': -122.23
+}
+
+# Tworzenie DataFrame z nowymi danymi
+new_property_df = pd.DataFrame([new_property])
+
+# Skalowanie danych wejściowych przy uzyciu tego samego skalera użytego do trenowania modelu
+scaled_new_property = scaler.transform(new_property_df)
+
+# Prognozowanie ceny przy użyciu wytrenowanego modelu
+preducted_price = model.predict(scaled_new_property)
+
+# Wyświetlenie ceny prognozowanej
+print(f"Prognozowana cena nieruchomości: {preducted_price[0][0]:.3f} (* 100.000 USD)")
