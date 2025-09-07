16b053339f7feae42a1f45484d42239f1fbbe8b5
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-07 11:18:58 +0200

    Niedziela

--- 4_flask.py
+++ 4_flask.py
@@ -1,10 +1,41 @@
+import joblib
+import numpy as np
 from flask import Flask, render_template, request
+from PIL import Image
 
 app = Flask(__name__)
+model = joblib.load('mnist_model.pkl')
 
 @app.route('/', methods=['GET', 'POST'])
 def digit():
-    return render_template("digit.html")
+    prediction = ''
+    if request.method == 'POST':
+        file = request.files['image']
+
+        img = Image.open(file).convert('L')
+        img = img.resize((28, 28))
+
+        img_array = np.array(img)
+
+        binary_image = (img_array <= 127).astype(int)
+
+        # print(binary_image)
+        #
+        # print("\nWizualizacja w terminalu:")
+        # for row in binary_image:
+        #     line = ''
+        #     for pixel in row:
+        #         if pixel == 1:
+        #             line += '#'
+        #         else:
+        #             line += ' '
+        #     print(line)
+
+        img_vector = (255 - img_array).reshape(1, -1) / 255
+
+        prediction = model.predict(img_vector)[0]
+
+    return render_template("digit.html", prediction=prediction)
 
 
 if __name__ == '__main__':
