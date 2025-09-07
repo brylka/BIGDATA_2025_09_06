b55a99a0fd5358021eda83316b7c9b35b72b0f90
Bartosz Bryniarski <brylka@jelenia.com>
2025-09-07 11:27:34 +0200

    Niedziela

--- 4_flask.py
+++ 4_flask.py
@@ -10,29 +10,19 @@ model = joblib.load('mnist_model_.pkl')
 def digit():
     prediction = None
     if request.method == 'POST':
+        # Pobranie pliku
         file = request.files['image']
 
+        # Konwersja pliku graficznego na skale szarości
         img = Image.open(file).convert('L')
+        # Zmiana rozmiaru na 28x28
         img = img.resize((28, 28))
-
+        # Zmiana pliku graficznego na tablicę
         img_array = np.array(img)
-
-        binary_image = (img_array <= 127).astype(int)
-
-        # print(binary_image)
-        #
-        # print("\nWizualizacja w terminalu:")
-        # for row in binary_image:
-        #     line = ''
-        #     for pixel in row:
-        #         if pixel == 1:
-        #             line += '#'
-        #         else:
-        #             line += ' '
-        #     print(line)
-
+        # Stworzenie wektora
         img_vector = (255 - img_array).reshape(1, -1) / 255
 
+        # Predykcja - czyli zgadywanie co jest na obrazie
         prediction = model.predict(img_vector)[0]
 
     return render_template("digit.html", prediction=str(prediction))
